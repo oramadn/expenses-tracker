56e90758c85f194290be45f4540bb97264dda1b7
oramadn <111233764+oramadn@users.noreply.github.com>
2024-09-27 17:48:08 +0300

    Exp 11 split category options into main cateogry and subcategory (#5)

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,6 +7,7 @@ from .views import (
 
 urlpatterns = [
     path('', expenses_view.index, name='expense_list'),
+    path('get-subcategories/', categories_view.get_subcategories, name='get_subcategories'),
     # path('create/', expenses_views.expense_create, name='expense_create'),
     # path('<int:pk>/edit/', expenses_views.expense_update, name='expense_update'),
     # path('<int:pk>/delete/', expenses_views.expense_delete, name='expense_delete'),
--- expenses/views/categories_view.py
+++ expenses/views/categories_view.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_GET
+from expenses.models import Category
 
 def index(request):
     return HttpResponse('We are at the index of category')
+
+@require_GET
+def get_subcategories(request):
+    try:
+        category_id = request.GET.get('category_id')
+        if not category_id:
+            return JsonResponse({'error': 'Category ID is required'}, status=400)
+        
+        subcategories = Category.objects.filter(parent_id=category_id).values('id', 'name')
+        return JsonResponse(list(subcategories), safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
