7421dd4503a8b8da901355dc357cbd93e587ad7f
Omar Ramadan <oramadan@chaizer.com>
2024-09-27 17:47:30 +0300

    EXP-11 dynamically render subcategories based on chosen category

--- expenses/admin.py
+++ expenses/admin.py
@@ -3,4 +3,22 @@ from .models.expense import Expense
 from .models.category import Category
 from .models.recurrence import Recurrence
 
-admin.site.register([Expense, Category, Recurrence])
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+      if db_field.name == "category":
+          kwargs["queryset"] = Category.objects.filter(parent__isnull=True)
+      return super().formfield_for_foreignkey(db_field, request, **kwargs)
+  
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['category'].widget.attrs.update({
+            'x-model': 'category',
+            '@change': 'category = $event.target.value'
+        })
+        return form
+
+    class Media:
+        js = ('js/expense_form.js',)
+
+admin.site.register([Category, Recurrence])
