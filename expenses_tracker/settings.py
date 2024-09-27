24283957a638c4b3fb3ad45ac1476c8add68e062
Omar Ramadan <oramadan@chaizer.com>
2024-09-27 13:56:21 +0300

    EXP-14 added custom dashboard settings

--- expenses_tracker/settings.py
+++ expenses_tracker/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'expenses_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
