db417997a5a23117078283af56f831c9f6f196e5
Omar Ramadan <oramadan@chaizer.com>
2024-10-07 20:46:08 +0300

    EXP-15 removed recurrence model

--- expenses/models/expense.py
+++ expenses/models/expense.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 
 from .category import Category
-from .recurrence import Recurrence
 
 class Expense(models.Model):
   EXPENSE_TYPES = (
@@ -14,12 +13,11 @@ class Expense(models.Model):
   currency = models.CharField(max_length=3, choices=settings.CURRENCIES)
   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='expenses')
   subcategory = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_expenses', null=True, blank=True)
-  expense_type = models.CharField(max_length=10, choices=EXPENSE_TYPES)
-  recurrence = models.ForeignKey(Recurrence, on_delete=models.CASCADE, null=True, blank=True)
+  type = models.CharField(max_length=10, choices=EXPENSE_TYPES)
   description = models.TextField(blank=True, null=True)
   expense_date = models.DateTimeField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   
   def __str__(self):
-        return f"{self.expense_type}: {self.amount} {self.currency}"
+        return f"{self.type}: {self.amount} {self.currency}"
