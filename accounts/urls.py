d8bf2be02d4d0a0db66e7225d38f1a98ccb158f7
oramadn <111233764+oramadn@users.noreply.github.com>
2024-10-31 09:40:20 +0300

    EXP-18 added register, login and logout (#12)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    UserProfileView,
 )
-from .views import RegisterView, UserProfileView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UserProfileView.as_view(), name='profile'),
 ]
